a0b424a2cb08aef16d61cc128e79865cd1f0437a
realsunwich <64021193@up.ac.th>
2025-07-07 16:15:00 +0700

    ปรับใหม่

--- src/app/BookingStat/page.tsx
+++ src/app/BookingStat/page.tsx
@@ -1,11 +1,8 @@
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import StatsClient from "@/components/StatsClient";
 import { Box } from "@mui/material";
 
 export default function BookingStatPage() {
-    useEffect(() => {
-        document.title = "กำลังดึงข้อมูล...";
-    }, []);
 
     return (
         <Suspense fallback={<Box display="flex" flexDirection="row" alignItems="center" justifyContent="center" gap={2} marginTop={40}>กำลังดึงข้อมูล...</Box>}>
