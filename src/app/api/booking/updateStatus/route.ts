04ee281567a00d2f016beb1350cbbd7287244c97
realsunwich <64021193@up.ac.th>
2025-07-09 15:16:08 +0700

    ปรับนิดนึง

--- src/app/api/booking/updateStatus/route.ts
+++ src/app/api/booking/updateStatus/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-import { PrismaClient, Prisma } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import prisma from "@/app/lib/prisma";
 
 interface UpdateBookingPayload {
     bookingId: number;
